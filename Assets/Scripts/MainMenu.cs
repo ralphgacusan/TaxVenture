86d6af8cc2d2fc861623d4fe58eb78ece08899d2
Juztine Miguel Bayucan <167217785+Zecht17@users.noreply.github.com>
2026-09-12 00:21:34 +0800

    imported the 3d models added the menu fixed the missing assets
    imported the 3d models
    added the menu
    fixed the missing assets

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,48 @@
-using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour { 
-    public void GoToScene(String sceneName)
+public class MainMenu : MonoBehaviour
+{
+    public ToggleCamera cameraManager;
+
+    private void Update()
     {
-        SceneManager.LoadScene(sceneName);
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Camera activeCamera = Camera.main;
+
+                if (activeCamera == null)
+                {
+                    Debug.LogError("No Main Camera found!");
+                    return;
+                }
+
+                Ray ray = activeCamera.ScreenPointToRay(touch.position);
+
+                if (Physics.Raycast(ray, out RaycastHit hit))
+                {
+                    GameObject touchedObject = hit.collider.gameObject;
+
+                    switch (touchedObject.name)
+                    {
+                        case "play_button":
+                            cameraManager.SwitchToLevelSelector();
+                            break;
+
+                        case "tutorial_button":
+                            SceneManager.LoadScene("TutorialPage");
+                            break;
+
+                        case "settings_button":
+                            SceneManager.LoadScene("SettingsPage");
+                            break;
+                    }
+                }
+            }
+        }
     }
 }
